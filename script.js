b57bbb76d9b515443a62ac57ec8cb79e448a188c
matheuspavao2 <yyyyeqze@proton.me>
2026-09-12 15:17:15 -0300

    lista de exercícios

---
+++ script.js
@@ -0,0 +1,11 @@
+// EXERCÍCIO 1 - Usuário
+
+// let nome = prompt("Qual é o seu nome?")
+
+// let apenasLetras = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ ]+$/;
+
+// if (!nome || ! apenasLetras.test(nome)) {
+//     alert("Erro! Por favor, digite um nome válido. (contenha apenas letras)") 
+// } else  {
+//     alert(`Seja bem vindo, ${nome}!`)
+// }
